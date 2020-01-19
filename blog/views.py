b92697ae181f15f9af4871cf49910244991c7017
Diego <diegovarelajp@gmail.com>
2020-01-19 11:26:55 -0300

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+#Uma view é o lugar onde nós colocamos a "lógica" da nossa aplicação. Ela vai extrair informações do model que você criou e entregá-las a um template.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
