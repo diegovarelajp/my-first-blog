fd5c3943fab598e417539fe817d3e6a46aca6ccb
Diego <diegovarelajp@gmail.com>
2020-01-24 23:09:32 -0300

    Add view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views# importando todas as nossas views do aplicativo blog
 
 urlpatterns = [#abaixo atribuímos uma view chamada post_list à URL raiz
     path('', views.post_list, name='post_list'),#post_list é nome da url que será usada para identificar a view
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
