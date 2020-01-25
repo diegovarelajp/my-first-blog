bfe697720ef2708061ab6b65c4eef178a269fbb6
Diego <diegovarelajp@gmail.com>
2020-01-24 22:17:38 -0300

    Modified templates to display posts from database.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path # importando a função do django url 
 from . import views# importando todas as nossas views do aplicativo blog
 
-urlpatterns = [
+urlpatterns = [#abaixo atribuímos uma view chamada post_list à URL raiz
     path('', views.post_list, name='post_list'),#post_list é nome da url que será usada para identificar a view
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post#importamos o modelo para a view
 
 # Create your views here.
 #Uma view é o lugar onde nós colocamos a "lógica" da nossa aplicação. Ela vai extrair informações do model que você criou e entregá-las a um template.
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+def post_list(request):#posts é o nome do nosso QuerySet
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
